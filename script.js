2cb6dc980422e9835765acee8cc1a7d34107db51
Deekshant Yadav <d33kshant@gmail.com>
2022-02-04 23:39:54 +0530

    Deleted script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const speed = 0.5;
-const slide = document.querySelector(".certificates-slide");
-
-slide.addEventListener("wheel", (evt) => {
-    evt.preventDefault();
-    slide.scrollLeft += evt.deltaY * speed;
-});
