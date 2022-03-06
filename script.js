cee3f3f622e0d4c2b201df2d03a117e2314a7ff3
Deekshant Yadav <d33kshant@gmail.com>
2022-03-06 22:40:05 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+// const setDarkMode = (value) => {
+// 	if (value)
+// 		document.body.classList.replace('light', 'dark')
+// 	else document.body.classList.replace('dark', 'light')
+// 	localStorage.setItem('dark-mode', value)
+// }
+
+// const toggle = document.getElementById('toggle-theme')
+// const is_enabled = localStorage.getItem('dark-mode') || false
+
+// setDarkMode(is_enabled)
+// toggle.checked = is_enabled
+
+// toggle.addEventListener('change', (event)=>{
+// 	setDarkMode(event.target.checked)
+// })
