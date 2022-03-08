659c4fedb82991e943b794106a639f635222ef39
Deekshant Yadav <d33kshant@gmail.com>
2022-03-08 10:57:44 +0530

    Add social links and temp diable theming

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-var toggle = document.getElementById('toggle-theme')
-var darkMode = JSON.parse(localStorage.getItem('dark-mode'))
+// var toggle = document.getElementById('toggle-theme')
+// var darkMode = JSON.parse(localStorage.getItem('dark-mode'))
 
-function setDarkMode(enable) {
-	if (enable) {
-		document.body.classList.remove('light')
-		document.body.classList.add('dark')
-	} else {
-		document.body.classList.remove('dark')
-		document.body.classList.add('light')
-	}
-}
+// function setDarkMode(enable) {
+// 	if (enable) {
+// 		document.body.classList.remove('light')
+// 		document.body.classList.add('dark')
+// 	} else {
+// 		document.body.classList.remove('dark')
+// 		document.body.classList.add('light')
+// 	}
+// }
 
-setDarkMode(darkMode)
-toggle.checked = darkMode
+// setDarkMode(darkMode)
+// toggle.checked = darkMode
 
-toggle.addEventListener('change', (event)=>{
-	const value = event.target.checked
-	setDarkMode(value)
-	localStorage.setItem('dark-mode', value)
-})
+// toggle.addEventListener('change', (event)=>{
+// 	const value = event.target.checked
+// 	setDarkMode(value)
+// 	localStorage.setItem('dark-mode', value)
+// })
