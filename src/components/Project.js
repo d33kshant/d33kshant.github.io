1f69a90adf730b27df481c394d8c1eb975c0023d
Deekshant Yadav <d33kshant@gmail.com>
2022-04-07 22:23:46 +0530

    Remove img element

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,7 +3,6 @@ const Project = ({ title, description, screenshot, source, deployment }) => {
 		<div className="w-full flex flex-col justify-center items-center gap-4">
 			<h1 className="font-manrope font-bold text-2xl" >{title}</h1>
 			<p className="font-sans text-lg max-w-3xl text-center">{description}</p>
-			<img src={screenshot} alt={title} width={540} />
 			<div className="flex gap-2" >
 				<a href={source}>
 					Source Code
