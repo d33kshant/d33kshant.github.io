11bc99d81f6b01d2506f2938c0b3e58852ab5de6
Deekshant Yadav <d33kshant@gmail.com>
2022-04-27 22:42:18 +0530

    List all project from data

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,16 @@
+import data from '../data.json'
 import "../styles/Projects.css"
+import Project from './Project'
 
 const Projects = () => {
 	return (
-		<div>Projects</div>
+		<>
+		<div className="projects-grid">
+			{data.projects.map((project, index)=><Project key={index} {...project} />)}
+		</div>
+		<hr className='divider' />
+		<span className='project-section-footer'>Most of my projects are available on <a href='https://github.com/d33kshant?tab=repositories'>GitHub</a></span>
+		</>
 	)
 }
 
